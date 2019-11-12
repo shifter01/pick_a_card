dbf59b04874e1eb75e59d73c74fb3a642663ed19
Alex <alexey2027@rambler.ru>
2019-11-12 14:21:56 +0300

    with symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,3 +1,5 @@
+
+
 value = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A' ]
-suit = ['Dimonds', 'Hearts', 'Clubs', 'Spades']
-puts value.sample + " of " + suit.sample
+suit = ['♦', '♠', '♥ ', '♣']
+puts value.sample + " of " + suit.sample.encode('UTF-8')
